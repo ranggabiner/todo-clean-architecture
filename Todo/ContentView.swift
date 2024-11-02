733a5a4e8e0d0073b4ae3a7df556b82408fd2d51
Rangga Biner <ranggabiner@gmail.com>
2024-11-02 22:56:13 +0700

    Initial Commit

---
+++ Todo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Todo
+//
+//  Created by Rangga Biner on 02/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by Rangga Biner on 02/11/24.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
